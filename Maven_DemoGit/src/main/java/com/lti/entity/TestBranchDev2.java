f61d13024191af183677d07ef8f89ac79845c150
Akash3121 <jammulaakashreddy03@gmail.com>
2022-07-14 14:22:15 +0530

    adding new Dev2 branch

---
+++ Maven_DemoGit/src/main/java/com/lti/entity/TestBranchDev2.java
@@ -0,0 +1,8 @@
+package com.lti.entity;
+
+public class TestBranchDev2 {
+	
+	public static void main(String[] args) {
+		System.out.println("Dev2 Branch code in test Branch Dev 2");
+	}
+}
--- Maven_DemoGit/src/main/java/com/lti/entity/UserApp.java
+++ Maven_DemoGit/src/main/java/com/lti/entity/UserApp.java
@@ -4,5 +4,4 @@ public class UserApp {
 	public static void main(String[] args) {
 		System.out.println();
 	}
-
 }
